6c1f03a1c0b38a1c4cd1d35c2a08bd37205bcbd8
ElDainosor <eldainosor@gmail.com>
2018-04-06 06:18:47 +0000

    [1/2] vendor: Add 3 new fonts
    This will be used in future plans
    
    Change-Id: I55134f35fd2e7f098bb73dc33129ca1c3fe6f0a7

--- config.mk
+++ config.mk
@@ -55,4 +55,7 @@ PRODUCT_COPY_FILES += \
     vendor/themes/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
     vendor/themes/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
     vendor/themes/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
-    vendor/themes/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
+    vendor/themes/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf \
+    vendor/themes/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
+    vendor/themes/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
+    vendor/themes/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf
