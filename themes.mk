4fd6d1a81e6ea4bf7ec98886afe8911611ccd165
ElDainosor <eldainosor@gmail.com>
2019-08-18 10:35:47 +0700

    vendor: Let's add bariol on our system fonts
    Usable for Launcher and LS fonts
    
    Change-Id: If35b0017a3186ec0e6952d1bde57ddc2880fd378

--- themes.mk
+++ themes.mk
@@ -111,4 +111,51 @@ PRODUCT_COPY_FILES += \
     vendor/themes/fonts/PhantomBold/PhantomBold.ttf:system/fonts/PhantomBold-Regular.ttf \
     vendor/themes/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/themes/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
-    vendor/themes/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
+    vendor/themes/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf \
+    vendor/themes/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
+    vendor/themes/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableClock-Regular.ttf \
+    vendor/themes/fonts/fontage/AdamCGPro-Regular.ttf:system/fonts/AdamCGPro-Regular.ttf \
+    vendor/themes/fonts/fontage/AlexanaNeue-Regular.ttf:system/fonts/AlexanaNeue-Regular.ttf \
+    vendor/themes/fonts/fontage/AlienLeague-Regular.ttf:system/fonts/AlienLeague-Regular.ttf \
+    vendor/themes/fonts/fontage/Azedo-Light.ttf:system/fonts/Azedo-Light.ttf \
+    vendor/themes/fonts/fontage/BigNoodleTilting-Italic.ttf:system/fonts/BigNoodleTilting-Italic.ttf \
+    vendor/themes/fonts/fontage/BigNoodleTilting-Regular.ttf:system/fonts/BigNoodleTilting-Regular.ttf \
+    vendor/themes/fonts/fontage/Biko-Regular.ttf:system/fonts/Biko-Regular.ttf \
+    vendor/themes/fonts/fontage/Blern-Regular.ttf:system/fonts/Blern-Regular.ttf \
+    vendor/themes/fonts/fontage/CoCoBiker-Regular.ttf:system/fonts/CoCoBiker-Regular.ttf \
+    vendor/themes/fonts/fontage/Fester-Medium.ttf:system/fonts/Fester-Medium.ttf \
+    vendor/themes/fonts/fontage/GinoraSans-Regular.ttf:system/fonts/GinoraSans-Regular.ttf \
+    vendor/themes/fonts/fontage/Inkferno-Regular.ttf:system/fonts/Inkferno-Regular.ttf \
+    vendor/themes/fonts/fontage/Instruction-Regular.ttf:system/fonts/Instruction-Regular.ttf \
+    vendor/themes/fonts/fontage/JackLane-Regular.ttf:system/fonts/JackLane-Regular.ttf \
+    vendor/themes/fonts/fontage/Metropolis1920-Regular.ttf:system/fonts/Metropolis1920-Regular.ttf \
+    vendor/themes/fonts/fontage/Monad-Regular.ttf:system/fonts/Monad-Regular.ttf \
+    vendor/themes/fonts/fontage/Noir-Regular.ttf:system/fonts/Noir-Regular.ttf \
+    vendor/themes/fonts/fontage/North-Regular.ttf:system/fonts/North-Regular.ttf \
+    vendor/themes/fonts/fontage/OutrunFuture-Regular.ttf:system/fonts/OutrunFuture-Regular.ttf \
+    vendor/themes/fonts/fontage/Qontra-Regular.ttf:system/fonts/Qontra-Regular.ttf \
+    vendor/themes/fonts/fontage/Riviera-Regular.ttf:system/fonts/Riviera-Regular.ttf \
+    vendor/themes/fonts/fontage/FoxAndCat-Regular.ttf:system/fonts/ThemeableDate-fc.ttf \
+    vendor/themes/fonts/fontage/FoxAndCat-Regular.ttf:system/fonts/ThemeableOwner-fc.ttf \
+    vendor/themes/fonts/fontage/TheOutbox-Regular.ttf:system/fonts/TheOutbox-Regular.ttf \
+    vendor/themes/fonts/fontage/Union-Regular.ttf:system/fonts/Union-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Abel-Regular.ttf:system/fonts/Abel-Regular.ttf \
+    vendor/themes/fonts/fontagev2/AdventPro-Regular.ttf:system/fonts/AdventPro-Regular.ttf \
+    vendor/themes/fonts/fontagev2/ArchivoNarrow-Regular.ttf:system/fonts/ArchivoNarrow-Regular.ttf \
+    vendor/themes/fonts/fontagev2/AutourOne-Regular.ttf:system/fonts/AutourOne-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Bariol-Regular.ttf:system/fonts/Bariol-Regular.ttf \
+    vendor/themes/fonts/fontagev2/BadScript-Regular.ttf:system/fonts/BadScript-Regular.ttf \
+    vendor/themes/fonts/fontagev2/CherrySwash-Regular.ttf:system/fonts/CherrySwash-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Codystar.ttf:system/fonts/Codystar.ttf \
+    vendor/themes/fonts/fontagev2/IBMPlexMono.ttf:system/fonts/IBMPlexMono.ttf \
+    vendor/themes/fonts/fontagev2/IBMPlexMono-Light.ttf:system/fonts/IBMPlexMono-Light.ttf \
+    vendor/themes/fonts/fontagev2/Jura-Regular.ttf:system/fonts/Jura-Regular.ttf \
+    vendor/themes/fonts/fontagev2/KellySlab-Regular.ttf:system/fonts/KellySlab-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Pompiere-Regular.ttf:system/fonts/Pompiere-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Raleway-Light.ttf:system/fonts/Raleway-Light.ttf \
+    vendor/themes/fonts/fontagev2/ReemKufi-Regular.ttf:system/fonts/ReemKufi-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Satisfy-Regular.ttf:system/fonts/Satisfy-Regular.ttf \
+    vendor/themes/fonts/fontagev2/SeaweedScript-Regular.ttf:system/fonts/SeaweedScript-Regular.ttf \
+    vendor/themes/fonts/fontagev2/SedgwickAveDisplay-Regular.ttf:system/fonts/SedgwickAveDisplay-Regular.ttf \
+    vendor/themes/fonts/fontagev2/Vibur.ttf:system/fonts/Vibur.ttf \
+    vendor/themes/fonts/fontagev2/Voltaire.ttf:system/fonts/Voltaire.ttf
