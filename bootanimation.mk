680fc59eede35852ab74dc89a1fc7c080b19f17d
azaharuddin <mdazahar.2008@gmail.com>
2020-07-08 04:41:27 +0530

    bootanimation: kenzo: screen-resolution correction...
    Change-Id: Iddac0f2e5f66aa2116d451d2ac33c8ab1f74394b

--- bootanimation.mk
+++ bootanimation.mk
@@ -2,11 +2,11 @@
 
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
 
-ifneq ($(filter raphael enchilada lavender mido wayne whyred platina ginkgo vince twolip beryllium violet X00T RMX1801 tissot x2 jasmine_sprout sirius grus,$(du_device)),)
+ifneq ($(filter raphael enchilada lavender mido wayne whyred platina ginkgo vince twolip beryllium violet X00T RMX1801 tissot x2 jasmine_sprout sirius grus kenzo,$(du_device)),)
 scr_resolution := 1080
 endif
 
-ifneq ($(filter ysl riva land santoni X00P kenzo,$(du_device)),)
+ifneq ($(filter ysl riva land santoni X00P,$(du_device)),)
 scr_resolution := 720
 endif
 
