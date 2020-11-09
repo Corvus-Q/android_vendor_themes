23c5d7f30e86f59e3bfbcd7454723f8730ac6869
Ritzz <riteshm321@gmail.com>
2020-11-09 09:08:41 +0100

    Bring Back Themes - Drop Bootanimation as it moved to vendor/corvus - Comment Fod For Now - Refractor to Corvus

--- common.mk
+++ common.mk
@@ -2,13 +2,10 @@
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
-ifeq ($(EXTRA_FOD_ANIMATIONS),true)
-DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/fod
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/fod
-endif
-
-# Boot animation
-include vendor/themes/bootanimation.mk
+#ifeq ($(EXTRA_FOD_ANIMATIONS),true)
+#DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/fod
+#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/fod
+#endif
 
 # Overlays
 include vendor/themes/overlays.mk
